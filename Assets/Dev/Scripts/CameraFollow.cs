022e995421cda8164d3fe7a25774909eb5762438
Ahmet TOPAK <75997899+ahmettopak@users.noreply.github.com>
2023-06-11 21:26:01 +0300

    Create Project

---
+++ Assets/Dev/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform target; // Takip edilecek hedefin referansı
+    public float smoothSpeed = 0.125f; // Takip etme hızı
+
+    private Vector3 offset;
+
+    private void Start() {
+        offset = transform.position - target.position;
+    }
+
+    private void LateUpdate() {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = smoothedPosition;
+    }
+}
---
+++ Assets/Dev/Scripts/PlayerMovementController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField] VariableJoystick variableJoystick;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rotateSpeed;
+   
+    private Animator animator;
+
+    private void Start() {
+        animator = GetComponent<Animator>(); 
+    }
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(variableJoystick.Horizontal, 0f, variableJoystick.Vertical);
+
+        if (variableJoystick.Horizontal != 0 || variableJoystick.Vertical != 0) {
+           
+            animator.SetBool("isRun", true);
+
+            //Set anim speed
+            if (Math.Abs(variableJoystick.Vertical) > Math.Abs(variableJoystick.Horizontal)) {
+                animator.speed = Math.Abs(variableJoystick.Vertical);
+            }
+            else {
+                animator.speed = Math.Abs(variableJoystick.Horizontal);
+            }
+            
+        }
+        else {
+            animator.SetBool("isRun", false);
+            animator.speed = Math.Abs(1);
+        }
+        transform.Translate(movement / moveSpeed);
+        transform.Rotate(0f, variableJoystick.Horizontal * rotateSpeed, 0);
+    }
+}
